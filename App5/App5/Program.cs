b5b9a349a7bca7f76488bfb1c25c1ac0a013c464
Aries Zhang <arieszhang@microsoft.com>
2024-11-04 10:40:08 +1100

    Add example code

---
+++ App5/App5/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml;
+using System;
+using System.Threading;
+using WinRT;
+
+namespace App5
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            ComWrappersSupport.InitializeComWrappers();
+            Application.Start((p) =>
+            {
+                DispatcherQueueSynchronizationContext context = new(DispatcherQueue.GetForCurrentThread());
+                SynchronizationContext.SetSynchronizationContext(context);
+                _ = new App();
+            });
+        }
+    }
+}
